95f3cb954c7abab67f2135cbc05cf7126a785a5a
Romain Bohdanowicz <romain.bohdanowicz@formation.tech>
2023-05-25 16:59:18 +0200

    Syntaxe Angular

--- Angular/src/app/app.module.ts
+++ Angular/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HelloComponent } from './hello/hello.component';
+import { FilterComponent } from './filter/filter.component';
+import { CounterComponent } from './counter/counter.component';
+import { UserFormComponent } from './user-form/user-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HelloComponent,
+    FilterComponent,
+    CounterComponent,
+    UserFormComponent
   ],
   imports: [
     BrowserModule,
---
+++ Angular/src/app/user-form/user-form.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-user-form',
+  templateUrl: './user-form.component.html',
+  styleUrls: ['./user-form.component.css']
+})
+export class UserFormComponent {
+  user = {
+    name: 'Romain',
+    email: 'romain.bohdanowicz@formation.tech'
+  }
+}
