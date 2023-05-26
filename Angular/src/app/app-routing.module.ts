7601c25f53d27962efcec76c940d96555fa1600b
Romain Bohdanowicz <romain.bohdanowicz@formation.tech>
2023-05-26 15:33:02 +0200

    Fin de formation

--- Angular/src/app/app-routing.module.ts
+++ Angular/src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { ExamplesComponent } from './examples/examples.component';
+import { ExTodosComponent } from './ex-todos/ex-todos.component';
 
 const routes: Routes = [{
   path: '', // === URL sans le slash de départ
@@ -9,6 +10,9 @@ const routes: Routes = [{
 }, {
   path: 'examples',
   component: ExamplesComponent,
+}, {
+  path: 'todos',
+  component: ExTodosComponent,
 }];
 
 @NgModule({
--- Angular/src/app/ex-todos/ex-todos.component.ts
+++ Angular/src/app/ex-todos/ex-todos.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { Todo } from './todo.model';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-ex-todos',
   templateUrl: './ex-todos.component.html',
-  styleUrls: ['./ex-todos.component.css']
+  styleUrls: ['./ex-todos.component.css'],
 })
 export class ExTodosComponent {
   saisie = 'ABC';
@@ -19,20 +20,35 @@ export class ExTodosComponent {
     //   title: 'XYZ',
     //   completed: true,
     // }
-  ]
+  ];
+
+  // Dans l'idéal on injecte HttpClient dans un service intermédiaire
+  // qui lui est injecté dans le composant (permet de créer une abstraction)
+  constructor(private httpClient: HttpClient) {
+    httpClient
+      .get<Todo[]>('http://localhost:8000/api/todos')
+      .subscribe((data) => {
+        this.todos = data;
+      });
+  }
 
   save(event: SubmitEvent) {
     event.preventDefault();
-    this.todos.push({
-      _id: Math.random(),
-      title: this.saisie,
-      completed: false,
-    });
-    this.saisie = '';
+    const body = { title: this.saisie, completed: false };
+    this.httpClient
+      .post<Todo>('http://localhost:8000/api/todos', body)
+      .subscribe((todo) => {
+        this.todos.push(todo);
+        this.saisie = '';
+      });
   }
 
   delete(todo: Todo) {
-    const indexToDelete = this.todos.indexOf(todo);
-    this.todos.splice(indexToDelete, 1);
+    this.httpClient
+      .delete(`http://localhost:8000/api/todos/${todo._id}`)
+      .subscribe(() => {
+        const indexToDelete = this.todos.indexOf(todo);
+        this.todos.splice(indexToDelete, 1);
+      });
   }
 }
